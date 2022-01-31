dca99fed5d1f68d704edc1d1c5111473fc653169
Advith Chegu <advil64@gmail.com>
2022-01-31 15:13:35 -0500

    changed groupme

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -34,7 +34,7 @@ const SignedInLinks = () => {
             <Nav.Link onClick={() => window.open("https://rutgers.campuslabs.com/engage/organization/SwimClub", "_blank")}>
                 GetInvolved <BoxArrowUpRight/>
             </Nav.Link>
-            <Nav.Link onClick={() => window.open("https://groupme.com/join_group/70392994/IHIGFq6X", "_blank")}>
+            <Nav.Link onClick={() => window.open("https://groupme.com/join_group/84911168/rjC8k4C0", "_blank")}>
                 GroupMe <BoxArrowUpRight/>
             </Nav.Link>
             </Nav>
